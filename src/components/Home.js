6510211dfcad2dfd74cae6974d55ec52f6e59331
sebastian medina <sebastianlopez_21@hotmail.com>
2023-01-03 22:17:36 -0500

    Creates Home Component

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home(){
+
+    return(
+        <div className="home-container">
+            <h1>
+                <span className="blue">
+                    Welcome  
+                    <br/>
+                </span>
+                TO THE BEST GLASSES STORE 
+            </h1>
+            <img src="https://cdn.shopify.com/s/files/1/0507/8954/8229/t/35/assets/warbyparkerreviewfeaturedbanner-1644926207240.jpg?v=1644926209"/>
+        </div>
+    )
+}
+
+export default Home;
